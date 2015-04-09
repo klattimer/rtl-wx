9b7ffb6fa88c429156b0169cafaa7adb03bddd8d
Karl Lattimer <karl@qdh.org.uk>
2015-04-09 23:21:44 +0100

    Owl CM160 support

--- src/rtl-433fm-decode.c
+++ src/rtl-433fm-decode.c
@@ -344,20 +344,11 @@ static int oregon_scientific_v3_parser(uint8_t bb[BITBUF_ROWS][BITBUF_COLS]) {
 		 fprintf(stderr,"Weather Sensor THGR810  Channel %d Temp: %3.1f°C  %3.1f°F   Humidity: %d%%\n", channel, temp_c, ((temp_c*9)/5)+32, humidity);
 	   }
 	   return 1;
-    } else if ((msg[0] != 0) && (msg[1]!= 0)) { //  sync nibble was found  and some data is present...
-// THIS CODE IS TEMPORARY AND VERY KLUDGEY DUE TO INABILITY TO PARSE OWL CHECKSUM
-// This code assumes the OWL sensor is connected to an oil burner circuit  (max 1000 watts) and validates the owl values based on that!
-	unsigned int current = get_owl_current(msg);
-	double total_current = get_owl_total_current(msg);
-        if ((current > 0) && (total_current > 0) && (current < 1000) && (total_current < 10000)) {
-		if (owl_msg_ok_callback != NULL) {
-			owl_msg_ok_callback(msg, 13, (float) current, total_current);
-			return 1;
-		} else
-//fprintf(stderr, "Power: %d (watts) Total: %7.4f (KWH)\n", current, total_current);
-fprintf(stderr, " Energy Sensor OWLCM119 Channel %d Current: %d (watts) Total: %7.4f (KWH)\n", msg[0]>>4, current, total_current);
-	} else if (owl_msg_error_callback != NULL)
-		owl_msg_error_callback(msg, 13, (float) current, total_current);		
+    } else if ((msg[0] == 0x20) && (msg[1] == 0x89)) { //  sync nibble was found  and some data is present...
+         float rawAmp = (msg[4] >> 4 << 8 | (msg[3] & 0x0f )<< 4 | msg[3] >> 4);
+	 fprintf(stderr, "current measurement reading value   = %.0f\n", rawAmp);
+         fprintf(stderr, "current watts (230v)   = %.0f\n", rawAmp /(0.27*230)*1000);
+         fprintf(stderr, "Message: "); for (i=0 ; i<14 ; i++) fprintf(stderr, "%02x ", msg[i]); fprintf(stderr, "\n");
     } else if ((msg[0] != 0) && (msg[1]!= 0)) { //  sync nibble was found  and some data is present...
 fprintf(stderr, "Message received from unrecognized Oregon Scientific v3 sensor.\n");
 fprintf(stderr, "Message: "); for (i=0 ; i<BITBUF_COLS ; i++) fprintf(stderr, "%02x ", msg[i]); fprintf(stderr, "\n");
